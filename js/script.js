5a6d31f1caab89d4b6ca574f60c0d595270bdf3f
kenneth-gouw <159185824+kenneth-gouw@users.noreply.github.com>
2024-05-29 18:49:48 +0000

    commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Kai Nguyen All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kai Nguyen
+// Created on: May 2024
 // This file contains the JS functions for index.html
+"use strict"
+
+/**
+ * Check service worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit-6-04-kenneth-gouw-4/sw.js", {
+    scope: "/ICS2O-Unit-6-04-kai-nguyen-4/",
+  })
+}
+
+window.onload = function () {
+  const params = new URLSearchParams(document.location.search)
+
+  const radius = params.get("radius")
+
+  if (radius >= 0) {
+    const volume = 4 / 3 * Math.PI * (radius ** 3)
+    document.getElementById("radiusAnswer").innerHTML = radius + " mm<sup>3</sup> <br /><br />"
+    document.getElementById("answer").innerHTML = "the volume of the sphere is: " + volume.toFixed(2) + "mm<sup>3</sup>"
+  } else {
+    document.getElementById("answer").innerHTML = "Invalid number"
+  }
+}
